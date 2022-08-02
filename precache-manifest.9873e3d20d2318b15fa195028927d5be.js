c0d7be1e501360c77b1090c432315944e1a7f280
Minjae Kim <widiba03304@gmail.com>
2022-08-02 21:45:42 +0900

    Updates

---
+++ precache-manifest.9873e3d20d2318b15fa195028927d5be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6328b2aa432f371f0742",
+    "url": "/meetingplace/static/css/main.bf99fe3c.chunk.css"
+  },
+  {
+    "revision": "6328b2aa432f371f0742",
+    "url": "/meetingplace/static/js/main.6328b2aa.chunk.js"
+  },
+  {
+    "revision": "75aa2d861595ad62f4c5",
+    "url": "/meetingplace/static/js/1.75aa2d86.chunk.js"
+  },
+  {
+    "revision": "8ab63f116470edd4d149",
+    "url": "/meetingplace/static/js/2.8ab63f11.chunk.js"
+  },
+  {
+    "revision": "b48cea79dd6861258e64",
+    "url": "/meetingplace/static/js/runtime~main.b48cea79.js"
+  },
+  {
+    "revision": "5f2b1d22f41b5c34f4fcdbf02ca0ba55",
+    "url": "/meetingplace/index.html"
+  }
+];
